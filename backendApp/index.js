e1ebb6c55be5de9b35bc93ec9da048123175a1de
alanniedecena <alannie.khem.decena@gmail.com>
2023-03-14 21:12:04 +1300

    yup

--- backendApp/index.js
+++ backendApp/index.js
@@ -7,6 +7,9 @@ const port = 3000 //set the port to 3000
 
 app.use('/', express.static('public'))
 
+
+
+
 // app.get('/', (req, res) => { //App.get is a binding for an endpoint.
 //     res.send('Hello World!')
 // })
@@ -22,6 +25,35 @@ app.listen(port, () => {
 console.log(`Example applistening at http://localhost:${port}`)
 })
 
+
+//-----------------------------------------------------------------------//
 //app 1 port 1 need to listen (labEx) normally 4 digit port number eg 3001, 3002
 
-//MVC, model, view is the controler, the controller is data handerler
+// const app1 = express() 
+// const port1 = 3001
+// app1.use('/', express.static('public'))
+// app1.listen(port1, () => {
+//     console.log(`Example applistening at http://localhost:${port1}`)
+//     })
+    
+// app1.use('/mytest', testRoute);
+
+// const app2 = express() 
+// const port2 = 3002
+// app2.use('/', express.static('public'))
+// app2.listen(port2, () => {
+//     console.log(`Example applistening at http://localhost:${port2}`)
+//     })
+    
+// app2.use('/mytest', testRoute);
+
+// const app3 = express() 
+// const port3 = 3003
+// app3.use('/', express.static('public'))
+// app3.listen(port3, () => {
+//     console.log(`Example applistening at http://localhost:${port3}`)
+//     })
+    
+// app3.use('/mytest', testRoute);
+
+//-----------------------------------------------------------------------//
--- backendApp/routes/myTestRoute.js
+++ backendApp/routes/myTestRoute.js
@@ -9,4 +9,8 @@ router.get('/test2', (req, res) => {
     res.send('Second test')
 })
 
+router.get('/test3', (req, res) => {
+    res.send('3rd test')
+})
+
 module.exports = router;
