642565b63abffc86fff21ff62a782a8566c962a3
alanniedecena <alannie.khem.decena@gmail.com>
2023-03-14 19:15:10 +1300

    :x

--- backendApp/index.js
+++ backendApp/index.js
@@ -1,1 +1,27 @@
-console.log('hi')
+
+const testRoute = require('./routes/myTestRoute');
+
+const express = require('express')//require the express package
+const app = express() //storing express package (app is our webserver)
+const port = 3000 //set the port to 3000
+
+app.use('/', express.static('public'))
+
+// app.get('/', (req, res) => { //App.get is a binding for an endpoint.
+//     res.send('Hello World!')
+// })
+
+// app.get('/test', (req, res) => {
+//     res.send('This is a test')
+//     })
+
+app.use('/mytest', testRoute);
+
+
+app.listen(port, () => {
+console.log(`Example applistening at http://localhost:${port}`)
+})
+
+//app 1 port 1 need to listen (labEx) normally 4 digit port number eg 3001, 3002
+
+//MVC, model, view is the controler, the controller is data handerler
